7cc6ab2b4e920cd55ef989dc858e6ebba10b52b4
av8ta <av8ta@protonmail.com>
2022-08-03 08:44:54 +0800

    add configuration with rc

--- bin.js
+++ bin.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 import { render } from './index.js'
-import { join } from 'path'
 
-render(join(process.cwd(), 'input'), join(process.cwd(), 'output'), false)
-// render(join(process.cwd(), 'node_modules'), join(process.cwd(), 'output'), true)
+const args = process.argv.filter(arg => !arg.startsWith('--'))
+
+const folder = args[2] && args[2] !== '.' ? args[2] : process.cwd()
+const output = args[3] && args[3] !== '.' ? args[3] : folder
+console.warn('input dir:', folder); console.warn('output dir:', output);
+
+await render(folder, output)
--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 import pull from 'pull-stream'
 import { read, write } from 'pull-files'
-import { readFile } from 'fs/promises'
-import { join } from 'path'
 import { renderMarkdown } from './remark.js'
+import path from 'node:path'
+import rc from 'rc'
 
-export async function render(inputDir = process.cwd(), outputDir = process.cwd(), cssInline = true) {
-  if (inputDir === outputDir) outputDir = join(outputDir, 'output')
+export async function render(inputDir = process.cwd(), outputDir = process.cwd()) {
+  if (inputDir === outputDir) outputDir = path.join(outputDir, 'output')
   console.warn('inputDir ', inputDir); console.warn('outputDir', outputDir)
 
-  const styles = await tryReadFile(inputDir, 'styles.css')
-  const theme = await tryReadFile(inputDir, 'theme.css')
+  const config = rc('mdsg', {})
+  console.warn(JSON.stringify(config, null, 2))
 
-  const css = [
-    'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/github-dark.min.css',
-    '/css/fonts.css',
-    '/css/styles.css',
-    '/css/theme.css',
-  ]
-  const style = [styles, theme]
+  /**
+   * if injectCss is false; config.css is rendered as <link rel="stylesheet" href="/..."> in head tag
+   * one line for each url in the array. order matters so put your theme css file last
+   * 
+   * if injectCss is true; the config.assets directory has all css files concatenated
+   * together lexicographically sorted by filename and inserted into style tags in head tag
+   */
+  const css = config.injectCss ? undefined : config.css
+  const style = config.injectCss && config.assets ? await concatenateCss(config.assets) : undefined
 
+  // read markdown and process with remark plugins
   pull(
-    read(join(inputDir, '**/*.md')),
+    read(path.join(inputDir, '**/*.md')),
     pull.asyncMap(async (file, callback) => {
       const content = await renderMarkdown(file.data, { css, style })
       file.data = content.value
@@ -32,14 +35,35 @@ export async function render(inputDir = process.cwd(), outputDir = process.cwd()
       else console.log('Finished writing to', outputDir)
     })
   )
+  // copy assets directory to output directory
+  if (config.assets) await outputAssets(config.assets, outputDir)
 }
 
-async function tryReadFile(directory, file) {
-  const filePath = join(directory, file)
-  try {
-    return await readFile(filePath, 'utf8')
-  } catch (error) {
-    console.error(`Error reading ${filePath}`)
-    return null
-  }
+async function concatenateCss(directory) {
+  return new Promise((resolve, reject) => {
+    pull(
+      read(path.join(directory, '**/*.css')),
+      pull.collect((error, files) => {
+        if (error) reject('Error concatenating css', error)
+        const sorted = files.sort((a, b) => a.path.localeCompare(b.path))
+        const css = sorted.map(file => file.data.toString('utf-8'))
+        resolve(css)
+      })
+    )
+  })
+}
+
+async function outputAssets(assets, outputDir) {
+  const assetsDirname = assets.split(path.sep)
+  console.log('assets path', assets)
+  console.log('path.basename', path.basename(assets))
+  console.log('assetsDirname', assetsDirname)
+  pull(
+    read(path.join(assets, '**/*.*')),
+    pull.through(console.log),
+    write(path.join(outputDir, path.basename(assets)), error => {
+      if (error) console.error('Error copying assets', error)
+      else console.log('Finished copying assets to', outputDir)
+    })
+  )
 }
